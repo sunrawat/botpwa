f7de7a8cf63cce5b891f723c6b07dfb518cd3f30
Suraj Rawat <srawat@dminc.com>
2017-10-10 13:19:08 +0530

    first commit added for the branhc .........................s

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 var cacheName = 'JiraBotv1';
 var filesToCache = [];
 
-self.addEventListener('install', function(e) {
+this.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
@@ -10,10 +10,10 @@ self.addEventListener('install', function(e) {
     })
   );
 });
-self.addEventListener('activate', function(e) {
+this.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
 });
 
-self.addEventListener('fetch', function(event) {
+this.addEventListener('fetch', function(event) {
   // Do something interesting with the fetch here
 });
