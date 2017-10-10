a33e2405bdb18c6d3622d994d0e7d8a0a6041a3d
Suraj Rawat <srawat@dminc.com>
2017-10-10 17:31:01 +0530

    first commit added for the branhc .........................s

--- js/app.js
+++ js/app.js
@@ -1,18 +1,4 @@
-if("serviceWorker" in navigator){
 
-	navigator.serviceWorker
-		.register('./sw.js' , {scope : './'})
-		.then(function(registeration){
-			console.log("serviceWorker registeration success !");
-		})
-		.catch(function(err){
-			console.log("serviceWorker notsuccess !" + err);
-		});
-
-
-		//document.getElementById('load').src = "http://manifesto.co.uk/wp-content/uploads/2014/06/happy-user-experience-design.jpg";
-
-}
 
 window.setInterval(function(){
 	if(navigator.onLine){
--- sw.js
+++ sw.js
@@ -19,15 +19,45 @@ this.addEventListener('install', function(e) {
   );
 });
 
-this.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
 
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have 2 stream.
+            var responseToCache = response.clone();
+
+            caches.open(cacheName)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
 this.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   debugger;
