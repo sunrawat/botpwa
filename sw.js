aa74c8998fd3c801e4c415fe9a039b80b152f787
Suraj Rawat <srawat@dminc.com>
2017-10-10 17:06:33 +0530

    first commit added for the branhc .........................s

--- sw.js
+++ sw.js
@@ -1,13 +1,14 @@
-var cacheName = 'JiraBotv4';
+var cacheName = 'JiraBot-v4';
 var filesToCache = [
   '/',
-  'index.html',
-  'css/style.css',
-  'js/jquery.min.js',
-  'js/app.js',
-  'sdk/aws-sdk-2.41.0.min.js',
-  'chat-background.jpg',
-  'DMI_Logo.svg.png',
+  '/index.html',
+  '/css/style.css',
+  '/fonts/fonts.css',
+  '/js/jquery.min.js',
+  '/js/app.js',
+  '/sdk/aws-sdk-2.41.0.min.js',
+  '/chat-background.jpg',
+  '/DMI_Logo.svg.png',
 ];
 this.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
